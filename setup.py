703d76bfb20e42065bb343319f10cffcf2960058
b-mcg <bmcg0890@gmail.com>
2014-04-03 15:53:39 -0700

    setup script

---
+++ setup.py
@@ -0,0 +1,43 @@
+# Imports
+from distutils.core import setup
+from shutil import copyfile, rmtree
+import os
+import iplookup
+
+
+
+pathname        =       os.getcwd()
+
+copyfile('iplookup.py', 'iplookup/iplookup')
+
+packages        =       ['iplookup']
+
+setup(  name            =       'iplookup',
+        version         =       '0.0.1',
+        description     =       'IP address information lookup utility.',
+        author          =       'b-mcg',
+        author_email    =       'bmcg0890@gmail.com',
+        url             =       'https://www.github.com/b-mcg/iplookup',
+        packages        =       packages,
+        package_dir     =       {'iplookup' : os.path.abspath(os.path.join(pathname, 'iplookup/'))},
+        scripts         =       ['iplookup/iplookup'], 
+        data_files      =       [('share/iplookup', ['README.md'])] 
+    )
+
+
+
+try:
+
+    os.remove(os.path.abspath(os.path.join(pathname, 'iplookup/iplookup')))
+
+except:
+    pass
+
+
+try:
+
+    rmtree(os.path.abspath(os.path.join(pathname, 'build/')))
+
+except:
+
+    pass
