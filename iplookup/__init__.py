9f7e7aefc3b415a62afe6a53f3eb150a7fc9f8ce
b-mcg <bmcg0890@gmail.com>
2014-04-03 15:55:44 -0700

    init file

---
+++ iplookup/__init__.py
@@ -0,0 +1,7 @@
+__title__       =       'iplookup'
+__version__     =       '0.0.1'
+__author__      =       'b-mcg'
+__license__     =       'GPL Version 3'
+__copyright__   =       'Copyright 2014-2016 b-mcg'
+
+from .ip_lookup import IP_Lookup
