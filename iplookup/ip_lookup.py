20dc0d289a203f90840b5b24d181ef066a85ddbd
b-mcg <bmcg0890@gmail.com>
2014-04-03 22:35:05 -0700

    Made the headers dictionary easier to read

--- iplookup/ip_lookup.py
+++ iplookup/ip_lookup.py
@@ -69,14 +69,14 @@ class IP_Lookup(object):
 
         # Create header dictionary to make the request look like it's from a web browser just incase there's query limiting
         headers     =       {
-                            'Host' : 'db-ip.com',
-                            'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0',
-                            'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                            'Accept-Language' : 'en-US,en;q=0.5',
-                            'Accept-Encoding' : 'gzip, deflate',
-                            'DNT' : 1,
-                            'Connection' : 'keep-alive',
-                            'Cache-Control' : 'max-age=0'
+                            'Host'              :   'db-ip.com',
+                            'User-Agent'        :   'Mozilla/5.0 (X11; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0',
+                            'Accept'            :   'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                            'Accept-Language'   :   'en-US,en;q=0.5',
+                            'Accept-Encoding'   :   'gzip, deflate',
+                            'DNT'               :   1,
+                            'Connection'        :   'keep-alive',
+                            'Cache-Control'     :   'max-age=0'
                             }
 
         # Check for possible errors and parse html returned from get request if no errors are found
